5976b39e4902a6e9f048c973f2ac7863765a4be2
mia <mia5@Umbc.edu>
2017-10-27 22:02:55 -0400

    added bad nav bar

--- js/index.js
+++ js/index.js
@@ -11,12 +11,24 @@ $(document).ready(function(){
     });
 });*/
 function lambdaConnect(){
+    var json =null;
+
     $.ajax({
         url: "https://l3k4n5rrs9.execute-api.us-east-2.amazonaws.com/prod/DigIt-Serenity",
         dataType: 'json',
-        headers: {"Access-Control-Allow-Origin":"*"},
         success: function(results){
-            console.log(results);
+            var parsed_result=JSON.parse(results);
+            printJSON(results);
         }
     });
 }
+
+function printJSON(results){
+
+}
+
+$(document).ready(function(){
+    $(".nav-tabs a").click(function(){
+        $(this).tab('show');
+    });
+});
