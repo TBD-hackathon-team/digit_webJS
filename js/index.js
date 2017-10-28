31c23cb28d78eb2820401a4d4077280413d2b9e3
mia <mia5@Umbc.edu>
2017-10-27 22:30:16 -0400

    adding dynamic div population from json

--- js/index.js
+++ js/index.js
@@ -18,17 +18,45 @@ function lambdaConnect(){
         dataType: 'json',
         success: function(results){
             var parsed_result=JSON.parse(results);
+            $('#jan').text(JSON.stringify(results));
             printJSON(results);
         }
     });
+}
+
+
+var jsonData = {
+    "Outcome": "Success",
+    "Message": null,
+    "Identity": "Request",
+    "Delay": 0.070943,
+    "FromCurrencySymbol": "BTC",
+    "FromCurrencyName": "Bitcoin",
+    "ToCurrencySymbol": "USD",
+    "ToCurrencyName": "United States dollar",
+    "Date": "10/20/2017",
+    "Time": "10:00:00 PM",
+    "Amount": 1500,
+    "Result": 8937000,
+    "Rate": 5958,
+    "Source": "SIX Financial Information"
+   };
+
+function tempJSONRead(){
+    var date= $("<div>").append($("<div>").text("Date: " + jsonData.Date));
+    var data= $("<div>").append($("<div>").text("1 "+jsonData.FromCurrencySymbol + " to " +jsonData.ToCurrencySymbol +": " + jsonData.Rate));
+    $('#today').append(date, data);
 }
 
 function printJSON(results){
 
 }
 
+
 $(document).ready(function(){
     $(".nav-tabs a").click(function(){
         $(this).tab('show');
     });
+
+    $('#jan').text("this is unique text for january weee");
 });
