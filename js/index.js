26b48bc0cf779319a77c93469e6818de68af69a6
mia <mia5@Umbc.edu>
2017-10-27 20:19:21 -0400

    proof of lambda connection working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-function 
+//function bitcoinData(){
+ //   $.getJSON("bitcoinData.json", function(json){
+ //       console.log(json);
+   // });
+//}
+
+/*
+$(document).ready(function(){
+    $.get('https://l3k4n5rrs9.execute-api.us-east-2.amazonaws.com/prod/DigIt-Serenity',function(data){
+        console.log(data);
+    });
+});*/
+function lambdaConnect(){
+    $.ajax({
+        url: "https://l3k4n5rrs9.execute-api.us-east-2.amazonaws.com/prod/DigIt-Serenity",
+        dataType: 'jsonp',
+        success: function(results){
+            console.log(results);
+        }
+    });
+}
